269e0e718387611c271dfb7755c181d750af4029
IlEvelynIl <184697870+IlEvelynIl@users.noreply.github.com>
2024-10-22 22:21:28 -0400

    rename seed variable

--- BO1-AntiCheat/src/display/verification.cpp
+++ BO1-AntiCheat/src/display/verification.cpp
@@ -12,7 +12,7 @@ using namespace std;
 #define DEFINE_RAND2 0
 #endif
 
-unsigned seed;
+unsigned init_time;
 unsigned seed1;
 unsigned uid1;
 unsigned seed2;
@@ -22,14 +22,14 @@ void Verification::init()
 {
 	auto now = chrono::system_clock::now();
 	auto epoch_time = chrono::time_point_cast<chrono::seconds>(now).time_since_epoch();
-	seed = epoch_time.count();
+	init_time = epoch_time.count();
 
-	mt19937 engine1(DEFINE_RAND1 * seed);
-	mt19937 engine2(DEFINE_RAND2 * seed);
+	mt19937 engine1(DEFINE_RAND1 * init_time);
+	mt19937 engine2(DEFINE_RAND2 * init_time);
 
-	seed1 = seed * DEFINE_RAND1;
+	seed1 = init_time * DEFINE_RAND1;
 	uid1 = engine1();
-	seed2 = seed * DEFINE_RAND2;
+	seed2 = init_time * DEFINE_RAND2;
 	uid2 = engine2();
 }
 
@@ -37,7 +37,7 @@ void Verification::print_verification()
 {
 	cout << "\n";
 	cout << "seed: " << seed1 << "\n";
-	cout << "uid1: " << seed << " | " << uid1 << "\n";
+	cout << "uid1: " << init_time << " | " << uid1 << "\n";
 	cout << "seed: " << seed2 << "\n";
-	cout << "uid2: " << seed << " | " << uid2 << "\n";
+	cout << "uid2: " << init_time << " | " << uid2 << "\n";
 }
